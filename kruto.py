02b1f6e6ceb109df19be00ba62f4f5a6e2a7ee88
Popov Georgii <georg7711@mail.ru>
2025-04-27 10:52:31 +0300

    APIшки

--- kruto.py
+++ kruto.py
@@ -1,18 +1,11 @@
-import asyncio
-import time
+import requests
 
 
-async def send_mail(num):
-    print('Улетело сообщение {}'.format(num))
-    await asyncio.sleep(1)
-    print('Сообщение {} доставлено'.format(num))
+api_url = 'http://api.open-notify.org/iss-now.json'
 
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
 
-async def main():
-    tasks = [send_mail(i) for i in range(10)]
-    await asyncio.gather(*tasks)
-
-
-start_time = time.time()
-asyncio.run(main())
-print(f'Время выполнения программы: {time.time() - start_time} с')
+if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)  # При другом коде ответа выводим этот код
