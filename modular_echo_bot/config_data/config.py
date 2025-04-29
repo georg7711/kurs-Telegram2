8a34e04306ec512d7c6c213fcab5be640bb38534
Popov Georgii <georg7711@mail.ru>
2025-04-29 12:08:18 +0300

    Модулярный эхо-бот и .git.example

---
+++ modular_echo_bot/config_data/config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Загружает переменные из .env
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN2")
+
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    return Config(tg_bot=TgBot(token=TOKEN))
