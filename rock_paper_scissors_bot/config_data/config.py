8d15d6da7a38d8d9d3c1c6d66c7db3333ddd7d3c
Popov Georgii <georg7711@mail.ru>
2025-04-29 13:25:37 +0300

    Камень, ножницы, бумага

---
+++ rock_paper_scissors_bot/config_data/config.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Загружает переменные из .env
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN4")
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=TOKEN))
